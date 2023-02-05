70abb744dfc0b1a2c1b933a4ce6390540ca4cd79
Diki Pangestu <41289010+DKPST404@users.noreply.github.com>
2023-02-05 15:16:14 +0700

    perhitungan fix

--- app/Controllers/perhitungan.php
+++ app/Controllers/perhitungan.php
@@ -67,35 +67,39 @@ class perhitungan extends BaseController
         $rumus_kriteria    = [];
         $hasil_perhitungan = [];
 
-        $data_kriteria = $this->M_Perhitungan->select("kriteria.id_kriteria, if(kriteria.jenis_kriteria = 'benefit', max(nilai_kriteria), min(nilai_kriteria)) as hasil")
+        $data_kriteria = $this->M_Perhitungan->select("kriteria.id_kriteria,if(kriteria.jenis_kriteria = 'benefit', max(nilai_kriteria), min(nilai_kriteria)) as hasil")
             ->join('kriteria', 'perhitungan.id_kriteria = kriteria.id_kriteria')
             ->join('datasiswa', 'perhitungan.id_siswa = datasiswa.id_siswa')
-            ->where('datasiswa.periode', $tahun)
-            ->groupBy('perhitungan.id_kriteria')
+            ->where('datasiswa.periode', $tahun)->where('kriteria.status_kriteria', 'aktif')
+            ->groupBy('kriteria.id_kriteria')
             ->get();
-
+        // dd($data_kriteria);
         foreach ($data_kriteria->getResult() as $kriteria) {
             $rumus_kriteria[$kriteria->id_kriteria] = $kriteria->hasil;
         }
 
         $data_siswa_ke_kriteria = $this->M_Perhitungan->join('kriteria', 'perhitungan.id_kriteria = kriteria.id_kriteria')
             ->join('datasiswa', 'perhitungan.id_siswa = datasiswa.id_siswa')
-            ->where('datasiswa.periode', $tahun)
-            ->get();
+            ->where('datasiswa.periode', $tahun)->where('kriteria.status_kriteria', 'aktif')
+            ->findall();
 
-        foreach ($data_siswa_ke_kriteria->getResult() as $value) {
-            if ($rumus_kriteria[$value->id_kriteria] > 0) {
-                if ($value->jenis_kriteria == 'benefit') {
-                    $perhitungan[$value->id_siswa][$value->id_kriteria] = (
-                        ($value->nilai_kriteria / $rumus_kriteria[$value->id_kriteria]) * ($value->bobot_kriteria / 100)
+        // dd($data_siswa_ke_kriteria);
+
+        foreach ($data_siswa_ke_kriteria as $value) {
+            if ($rumus_kriteria[$value['id_kriteria']] > 0) {
+                if ($value['jenis_kriteria'] == 'benefit') {
+                    $perhitungan[$value['id_siswa']][$value['id_kriteria']] = (
+                        ($value['nilai_kriteria'] / $rumus_kriteria[$value['id_kriteria']]) * ($value['bobot_kriteria'] / 100)
                     );
                 } else {
-                    $perhitungan[$value->id_siswa][$value->id_kriteria] = (
-                        ($rumus_kriteria[$value->id_kriteria] / $value->nilai_kriteria) * ($value->bobot_kriteria / 100)
+                    $perhitungan[$value['id_siswa']][$value['id_kriteria']] = (
+                        ($rumus_kriteria[$value['id_kriteria']] / $value['nilai_kriteria']) * ($value['bobot_kriteria'] / 100)
                     );
                 }
             }
         }
+
+        // dd($perhitungan);
 
         $data_siswa = $this->M_Perhitungan->join('datasiswa', 'perhitungan.id_siswa = datasiswa.id_siswa')
             ->where('datasiswa.periode', $tahun)
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -15,7 +15,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url() ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- <link href="<?= base_url() ?>/assets/css/sb-admin-2.min.css" rel="stylesheet"> -->
+    <link href="<?= base_url() ?>/assets/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom alert box SweetAlert -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
@@ -35,6 +36,7 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
         <!-- Sidebar -->
+        <!-- <ul class="navbar-nav bg-gradient-custom_warna sidebar sidebar-dark accordion toggled" id="accordionSidebar"> -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('home'); ?>">
